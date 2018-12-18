31e41afb32087857230612ea52d1c222a03a87f0
robertoelerojunior <robertoelerojunior@gmail.com>
2018-12-17 22:55:08 -0200

    Adding Ribbon.

--- src/main/java/robert/o/eurekasentence/DemoController.java
+++ src/main/java/robert/o/eurekasentence/DemoController.java
@@ -1,21 +1,15 @@
 package robert.o.eurekasentence;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.List;
-
 @RestController
 public class DemoController {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    @Autowired RestTemplate template;
 
     @GetMapping("/sentence")
     public @ResponseBody
@@ -29,14 +23,6 @@ public class DemoController {
     }
 
     public String getWord(String service) {
-        List<ServiceInstance> list = discoveryClient.getInstances(service);
-
-        if (list != null && list.size() > 0) {
-            URI uri = list.get(0).getUri();
-            if (uri != null) {
-                return (new RestTemplate()).getForObject(uri, String.class);
-            }
-        }
-        return null;
+        return template.getForObject("http://" + service, String.class);
     }
 }
