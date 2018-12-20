e5963e2ca7ff04722890fc7c42143179b6a11618
robertoelerojunior <robertoelerojunior@gmail.com>
2018-12-19 22:12:31 -0200

    Removing unused class.

--- src/main/java/robert/o/eurekasentence/dao/impl/WordDAOImpl.java
+++ src/main/java/robert/o/eurekasentence/dao/impl/WordDAOImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.client.RestTemplate;
-import robert.o.eurekasentence.dao.WordDAO;
 import robert.o.eurekasentence.domain.Word;
 
 public abstract class WordDAOImpl implements WordDAO {
