da491405b3a9b54adc0200b0d9bb193de6bb4da7
robertoelerojunior <robertoelerojunior@gmail.com>
2018-12-19 22:17:31 -0200

    Adding Feign Verb Client.

--- src/main/java/robert/o/eurekasentence/service/impl/SentenceServiceImpl.java
+++ src/main/java/robert/o/eurekasentence/service/impl/SentenceServiceImpl.java
@@ -2,11 +2,11 @@ package robert.o.eurekasentence.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import robert.o.eurekasentence.dao.AdjectiveClient;
 import robert.o.eurekasentence.dao.ArticleClient;
 import robert.o.eurekasentence.dao.NounClient;
 import robert.o.eurekasentence.dao.SubjectClient;
+import robert.o.eurekasentence.dao.VerbClient;
 import robert.o.eurekasentence.service.SentenceService;
 
 @Service
@@ -25,16 +25,12 @@ public class SentenceServiceImpl implements SentenceService {
     private SubjectClient subjectClient;
 
     @Autowired
-    private RestTemplate template;
-
-    private String getWord(String service) {
-        return template.getForObject("http://" + service, String.class);
-    }
+    private VerbClient verbClient;
 
     @Override
     public String getSentence() {
         return subjectClient.getWord() + " "
-                + getWord("VERB") + " "
+                + verbClient.getWord() + " "
                 + articleClient.getWord() + " "
                 + adjectiveClient.getWord() + " "
                 + nounClient.getWord() + ".";
